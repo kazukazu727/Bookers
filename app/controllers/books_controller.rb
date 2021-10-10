621ce7bdf1a113242da96682e5dc224d53deb693
kazukazu727 <kazuma.xdbxdbx@gmail.com>
2021-10-10 13:10:46 +0000

    [update]4-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,32 @@
 class BooksController < ApplicationController
   def new
-    @book=Book.new
+   @book=Book.new
   end
 
   def create
-    book=Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book=Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
   
   def index
     @books=Book.all
   end
-
+  
   def show
     @book=Book.find(params[:id])
   end
-
+  
   def edit
     @book=Book.find(params[:id])
   end
   
   def update
     book=Book.find(params[:id])
-    book.update(book_params)
+    @book.update(book_params)
     redirect_to book_path(book)
   end
   
