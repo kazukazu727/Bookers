e3f3923af60d7a2a878df7de92cad21dff293b1a
kazukazu727 <kazuma.xdbxdbx@gmail.com>
2021-10-11 06:13:44 +0000

    [add5]4-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,21 @@
 class BooksController < ApplicationController
-  def new
-   @book=Book.new
+  def index
+    @books=Book.all
+    @book=Book.new
   end
-
+  
   def create
     @book=Book.new(book_params)
+    @books=Book.all
     if @book.save
+       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       render :index
     end
   end
-  
-  def index
-    @books=Book.all
-  end
+
+
   
   def show
     @book=Book.find(params[:id])
@@ -25,9 +26,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book=Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(book)
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+       redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
   
   def destroy
@@ -38,7 +43,7 @@ class BooksController < ApplicationController
   
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
   
 end
